31241bf1b7b3397fa2a758cb1b1bc0623d2b4b9d
yangjianhua <yangjianhua@gmail.com>
2018-04-02 12:23:35 +0800

    modified:   flash_pre-k.py

--- flash_pre-k.py
+++ flash_pre-k.py
@@ -3,7 +3,10 @@ import random
 import time
 import sys
 
-lstAll = ['and', 'away', 'big', 'blue', 'can', 'come',
+# Get Words from http://www.sightwords.com/sight-words/dolch/
+# "Pre-K Dolch Sight Words"
+
+lstAll = ['a', 'and', 'away', 'big', 'blue', 'can', 'come',
     'down', 'find', 'for', 'funny', 'go', 'help', 'here', 'I',
     'in', 'is', 'it', 'jump', 'little', 'look', 'make', 'me',
     'my', 'not', 'one', 'play', 'red', 'run', 'said', 'see',
